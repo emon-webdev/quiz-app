230e5545fd606e2c61a39f45241765fd090e4f25
emon-webdev <emon.webdev@gmail.com>
2022-10-10 22:56:34 +0600

    first commit

---
+++ src/components/Topics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Topics = () => {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Topics;
