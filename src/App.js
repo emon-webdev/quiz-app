6342faeb6f35b4f2b84a6396cc4a3efee65c0cf4
emon-webdev <emon.webdev@gmail.com>
2022-10-10 23:10:55 +0600

    router set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Blog from "./components/Blog";
 import ErrorPage from "./components/ErrorPage";
+import Statistics from "./components/Statistics";
 import Topics from "./components/Topics";
 import Root from "./Root";
 
@@ -12,8 +14,20 @@ function App() {
       errorElement: <ErrorPage />,
       children: [
         {
+          path: "/",
+          element: <Topics />,
+        },
+        {
           path: "/topics",
           element: <Topics />,
+        },
+        {
+          path: "/statistics",
+          element: <Statistics />,
+        },
+        {
+          path: "/blog",
+          element: <Blog />,
         },
       ],
     },
